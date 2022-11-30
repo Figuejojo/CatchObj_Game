f8e5f19c2684c030a6853568029c6f904ef6b67e
José Jorge Figueroa Figueroa <figuejojo6@gmail.com>
2022-11-30 00:10:44 +0000

    chore(Clean): Delete things from Assignment and add .wav file

--- src/assignment.c
+++ src/assignment.c
@@ -10,11 +10,10 @@ void audio_once_main(void){
 
     // load a sample and give it a name.
 
-    amio_load_sample("vocal", "./samples/vocals.wav");
-
+    amio_load_sample("vocal", "./data/mixkit-magic-astral-sweep-effect-2629.wav");
     // we can choose to add an instance of the sample. We can either play it once or on a loop. Also we can change the volume (between 0 and 1).
 
-    amio_add_sample_instance("vocal", PLAY_ONCE, 0.9);
+    amio_add_sample_instance("vocal", PLAY_ONCE, 0.3);
 
     // update audio for changes to take effect.
 
@@ -22,32 +21,9 @@ void audio_once_main(void){
 
     // audio is not affected by pausing the main thread.
 
-    pausefor(1000);
-
-    // we can play multiple instances of the same sample at the same time
-
-    amio_add_sample_instance("vocal", PLAY_ONCE, 0.9);
-
-    amio_update_audio();
-
-    pausefor(4000);
+    pausefor(5000);
 
     // Destroy audio when exiting the program.
-
-    amio_destroy_audio();
-}
-
-void audio_loop_main(void){
-
-    amio_init_audio();
-
-    amio_load_sample("vocal", "./samples/vocals.wav");
-
-    amio_add_sample_instance("vocal", PLAY_LOOP, 0.9);
-
-    amio_update_audio();
-
-    pausefor(5000);
 
     amio_destroy_audio();
 }
@@ -82,9 +58,6 @@ int main(void)
 
     printf("\nNow running audio_once_main\n");
     audio_once_main();
-
-    printf("\nNow running audio_loop_main\n");
-    audio_loop_main();
 
     /* remove the display */
     closegraph();
